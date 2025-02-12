df6770e1ec513812431cd8ec924547a2ca5a8498
Lucas G. Lisboa <lglisboa1503@gmail.com>
2025-02-12 19:27:59 -0300

    fonts/Aptos

--- The Book of Shaders/fragment_shader.glsl
+++ The Book of Shaders/fragment_shader.glsl
@@ -1,6 +1,20 @@
-#version 450 core
+#version 460 core
 out vec4 FragColor;
 
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;  // Canvas size (width,height)
+uniform vec2 u_mouse;       // mouse position in screen pixels
+uniform float u_time;       // Time in seconds since load
+
+vec3 red();
+
 void main() {
-    FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    FragColor = vec4(red(), 1.0);
+}
+
+vec3 red() {
+    return vec3(1.0, 0.0, 0.0);
 }
